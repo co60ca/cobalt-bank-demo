6c05a05cf8cf083f627f2d05d425c745833470b6
Mae Kennedy <mae@co60.ca>
2020-05-11 19:12:35 -0400

    Updates

---
+++ precache-manifest.e569107de46b51bdce7be183a4761a41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94f3ef35b8168433630dd5370f31e4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2beae1819114ed095a6",
+    "url": "/static/css/main.739ddc11.chunk.css"
+  },
+  {
+    "revision": "38b5afb9c2c5277ebc79",
+    "url": "/static/js/2.d1f9970b.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/static/js/2.d1f9970b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2beae1819114ed095a6",
+    "url": "/static/js/main.e2c6a785.chunk.js"
+  },
+  {
+    "revision": "635bbbcd81598a25f53d",
+    "url": "/static/js/runtime-main.35b7217c.js"
+  }
+]);
